2879e07a21ba30c792f43ec9b2e8fa9cf672e18d
Bartlomiej Soter <bartlomiej.soter@shijigroup.com>
2022-04-22 19:42:17 +0200

    Step 3: Implement all tryParse[...] functions that parse string to: 1. Decimal - try to use standard System.Decimal.TryParse(...) method.    REMARKS:    Be careful about handling 'out' params in F#. See how it should be    done or ask tutor. 2. Currency - you can do it simply by pattern matching the input    string or by implementing more sophisticated approach using    reflection.
    FILES
      Implementation:
        FSharpWorkshop.Implementation/Finanses/Parsing.fs
      Unit tests:
        FSharpWorkshop.Implementation.Tests/TryParseCurrencyTests.fs
        FSharpWorkshop.Implementation.Tests/TryParseDecimalTests.fs
        FSharpWorkshop.Implementation.Tests/TryParseMoneyTests.fs

--- Enterprise/FSharpWorkshop.Implementation/ActivePatterns.fs
+++ Enterprise/FSharpWorkshop.Implementation/ActivePatterns.fs
@@ -1,6 +1,19 @@
 module FSharpWorkshop.Implementation.ActivePatterns
 
+open System.Text.RegularExpressions
 open Microsoft.FSharp.Core
 
 let (|TryRegexMatch|_|) (pattern: string) (input: string): Map<string, string> option =
-    failwith "not implemented"
+    let m = Regex.Match(input, pattern)
+    if not m.Success
+    then None
+    else
+        let groups =
+            m.Groups
+            |> Seq.cast<Group>
+        let map =
+            groups
+            |> Seq.filter (fun group -> group.Success)
+            |> Seq.map (fun group -> (group.Name, group.Value))
+            |> Map.ofSeq
+        Some map
